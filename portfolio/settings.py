cccf0d942658b3b57ae970407f6be265d1aac3a2
rhmahfuz <rh.mahfuz@yahoo.com>
2018-08-09 04:33:16 +0800

    Polish

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -122,9 +122,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
-STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'portfolio/static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/hellostatic/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 MEDIA_URL = '/media/'
